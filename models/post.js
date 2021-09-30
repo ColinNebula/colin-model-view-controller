4279bee48845feab5922b991eb803677f44eeec4
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2021-09-30 16:28:14 -0400

    connected to jawsdb

--- models/post.js
+++ models/post.js
@@ -7,7 +7,31 @@ class Post extends Model {
 
     }
 }
-
+// post class Extention
+class Post extends Model {
+    static upvote(body, models) {
+        return models.Vote.create({
+            user_id: body.user_id,
+            post_id: body.post_id
+        }).then(() => {
+            return Post.findOne({
+                where: {
+                    id: body.post_id
+                },
+                attributes: [
+                    'id',
+                    'post_url',
+                    'title',
+                    'created_at',
+                    [
+                        sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                        'vote_count'
+                    ]
+                ]
+            });
+        });
+    }
+}
 // create fields/columns for Post model
 Post.init(
     {

a9f3edb6dfd614648cbffbd5419adb5e6d4a5261
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2021-09-30 17:26:31 -0400

    added handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const apiRoutes = require('./api');
 const router = require('express').Router();
+const homeRoutes = require('./home-routes.js');
 
 
 // API routes
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();

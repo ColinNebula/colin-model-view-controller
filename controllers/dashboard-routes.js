79669a696149d85293829bdd5f2f1c66bea058a1
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2021-10-10 23:54:10 -0400

    added dashboard handlebars

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,49 +1,16 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment, Vote } = require('../models');
+const withAuth = require('../utils/auth');
 
-// get all posts for homepage
-router.get('/', (req, res) => {
-    console.log('req.session');
+// get all posts for dashboard
+router.get('/', withAuth, (req, res) => {
+    res.render('dashboard', { loggedIn: true });
+    console.log(req.session);
+    console.log('======================');
     Post.findAll({
-        attributes: [
-            'id',
-            'post_url',
-            'title',
-            'created_at',
-            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        ],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-        .then(dbPostData => {
-            const posts = dbPostData.map(post => post.get({ plain: true }));
-
-            res.render('homepage', { posts });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
-
-// get single post
-router.get('/post/:id', (req, res) => {
-    Post.findOne({
         where: {
-            id: req.params.id
+            user_id: req.session.user_id
         },
         attributes: [
             'id',
@@ -68,17 +35,8 @@ router.get('/post/:id', (req, res) => {
         ]
     })
         .then(dbPostData => {
-            if (!dbPostData) {
-                res.status(404).json({ message: 'No post found with this id' });
-                return;
-            }
-
-            const post = dbPostData.get({ plain: true });
-
-            res.render('single-post', {
-                post,
-                loggedIn: req.session.loggedIn
-            });
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('dashboard', { posts, loggedIn: true });
         })
         .catch(err => {
             console.log(err);
@@ -86,14 +44,45 @@ router.get('/post/:id', (req, res) => {
         });
 });
 
-// Get a login
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findByPk(req.params.id, {
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (dbPostData) {
+                const post = dbPostData.get({ plain: true });
 
-    res.render('login');
+                res.render('edit-post', {
+                    post,
+                    loggedIn: true
+                });
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,17 @@
-const apiRoutes = require('./api');
 const router = require('express').Router();
-const homeRoutes = require('./home-routes.js');
 
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
 // API routes
-router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+// Dashboard routes
+router.use('/dashboard', dashboardRoutes);
+router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-  res.status(404).end();
-});
+// router.use((req, res) => {
+//   res.status(404).end();
+// });
 
 module.exports = router;

21511bb168d5b48840636b6fa87272514cd1947a
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2021-10-20 14:23:38 -0400

    connect to jawsdb

--- models/post.js
+++ models/post.js
@@ -5,7 +5,7 @@ const sequelize = require('../config/connection');
 // post class Extention
 class Post extends Model {
     // post class Extends
-        
+
     static upvote(body, models) {
         return models.Vote.create({
             user_id: body.user_id,
@@ -14,57 +14,57 @@ class Post extends Model {
             return Post.findOne({
                 where: {
                     id: body.post_id
-                    },
+                },
                 attributes: [
                     'id',
                     'post_url',
                     'title',
                     'created_at',
-                        [
-                            sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-                            'vote_count'
-                        ]
-                        ]
-                    });
-                });
+                    [
+                        sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                        'vote_count'
+                    ]
+                ]
+            });
+        });
 
+    }
+}
+
+// create fields/columns for Post model
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isURL: true
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
             }
         }
-  
-        // create fields/columns for Post model
-        Post.init(
-            {
-                id: {
-                    type: DataTypes.INTEGER,
-                    allowNull: false,
-                    primaryKey: true,
-                    autoIncrement: true
-                },
-                title: {
-                    type: DataTypes.STRING,
-                    allowNull: false
-                },
-                post_url: {
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                    validate: {
-                        isURL: true
-                    }
-                },
-                user_id: {
-                    type: DataTypes.INTEGER,
-                    references: {
-                        model: 'user',
-                        key: 'id'
-                    }
-                }
-            },
-            {
-                sequelize,
-                freezeTableName: true,
-                underscored: true,
-                modelName: 'post'
-            }
-        )   
-   
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+)
+
 module.exports = Post;

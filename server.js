8873b47650652b798bc0314cfd28d9adb270c2aa
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2021-10-24 16:38:15 -0400

    updated server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
-const routes = require('./controllers/');
+
 
 
 //Port used
